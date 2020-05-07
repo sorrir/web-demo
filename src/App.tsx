04ef960d4d9a20a5932e04e3bf61f083ebae253f
David <david.moedinger@ketzu.net>
2020-05-07 13:44:32 +0200

    it's not pretty, but different.

--- src/App.tsx
+++ src/App.tsx
@@ -1,162 +1,199 @@
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import GridList from '@material-ui/core/GridList';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import * as _ from 'lodash';
-import React, { useState, useEffect } from 'react';
-import { barrier, BarrierEventTypes, BarrierPorts, barrier_state, Component, Configuration, configurationStep, createConnection, DSB, DSBEventTypes, DSB_Ports, DSB_state, Event, sensor, SensorEventTypes, SensorPorts, sensor_startstate, stateSpace, allConfigurationSteps, depGraphToDot, user, user_startstate, UserPorts, UserEventTypes } from "sorrir-framework";
+import React, {useState, useEffect} from 'react';
+import {
+    barrier,
+    BarrierEventTypes,
+    BarrierPorts,
+    barrier_state,
+    Component,
+    Configuration,
+    configurationStep,
+    createConnection,
+    DSB,
+    DSBEventTypes,
+    DSB_Ports,
+    DSB_state,
+    Event,
+    sensor,
+    SensorEventTypes,
+    SensorPorts,
+    sensor_startstate,
+    stateSpace,
+    allConfigurationSteps,
+    depGraphToDot,
+    user,
+    user_startstate,
+    UserPorts,
+    UserEventTypes
+} from "sorrir-framework";
 import './App.css';
-import { ComponentComp } from "./components/Component";
+import {ComponentComp} from "./components/Component";
 import TextField from '@material-ui/core/TextField';
+import {Grid, CssBaseline, Container} from "@material-ui/core";
 
 
-
-declare var Viz:any;
+declare var Viz: any;
 
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-    control: {
-      padding: theme.spacing(2),
-    },
-    nested: {
-      paddingLeft: theme.spacing(4),
-    },
-  }),
+    createStyles({
+        content: {
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto'
+        },
+        grow: {
+            flexGrow: 1,
+        },
+        inputColor: {
+            color: '#E2E2E2'
+        }
+    }),
 );
 
 const App: React.FC = () => {
 
-  useEffect(() => {
-    const script = document.createElement('script');
-  
-    script.src = "http://webgraphviz.com//viz.js";
-    script.async = true;
-  
-    document.body.appendChild(script);
-  
-    return () => {
-      document.body.removeChild(script);
-    }
-  }, []);
-  
-  const configuration:Configuration  = {
-    components: [barrier, DSB, sensor, user],
-    connections: [
-      createConnection(sensor, SensorPorts.TO_DSB, DSB, DSB_Ports.FROM_SENSOR),
-      createConnection(DSB, DSB_Ports.TO_BARRIER, barrier, BarrierPorts.FROM_DSB),
-      createConnection(DSB, DSB_Ports.TO_USER, user, UserPorts.FROM_DSB),
-      createConnection(user, UserPorts.TO_DSB, DSB, DSB_Ports.FROM_USER),
-    ]
-  }
-  
-  let startState = {
-    componentState: new Map([
-      [barrier, barrier_state] as [any, any],
-      [sensor, sensor_startstate] as [any, any],
-      [DSB, DSB_state] as [any, any],
-      [user, user_startstate] as [any, any],
-    ]),
-  }
-  
+    useEffect(() => {
+        const script = document.createElement('script');
 
-  const [configurationState, setConfigurationState] = useState(_.cloneDeep(startState));
-  const [svg, setSVG] = useState("");
-  const [spaceSize, setSpaceSize] = useState(10);
+        script.src = "http://webgraphviz.com//viz.js";
+        script.async = true;
 
-  function createStateSpace() {
-    var data = depGraphToDot(stateSpace(configuration, configurationState, allConfigurationSteps, spaceSize));
-    // Generate the Visualization of the Graph into "svg".
-    var svg = Viz(data, "svg");
-    setSVG(svg);
-  }
+        document.body.appendChild(script);
 
-  function reset() {
-    setConfigurationState(_.cloneDeep(startState));
-    setSVG("");
-  }
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, []);
 
-  function enqueueEvent(component:  Component<BarrierEventTypes, BarrierPorts> | Component<BarrierEventTypes | SensorEventTypes | DSBEventTypes | UserEventTypes, DSB_Ports> | Component<UserEventTypes, UserPorts>, event:  Event<BarrierEventTypes, BarrierPorts> & Event<BarrierEventTypes | SensorEventTypes | DSBEventTypes | UserEventTypes, DSB_Ports> & Event<UserEventTypes, UserPorts>) {
-    let newConfigurationState = {...configurationState};
-
-    const comp_state = newConfigurationState.componentState.get(component);
-    if (comp_state !== undefined) {
-      comp_state.events.push(event);
+    const configuration: Configuration = {
+        components: [barrier, DSB, sensor, user],
+        connections: [
+            createConnection(sensor, SensorPorts.TO_DSB, DSB, DSB_Ports.FROM_SENSOR),
+            createConnection(DSB, DSB_Ports.TO_BARRIER, barrier, BarrierPorts.FROM_DSB),
+            createConnection(DSB, DSB_Ports.TO_USER, user, UserPorts.FROM_DSB),
+            createConnection(user, UserPorts.TO_DSB, DSB, DSB_Ports.FROM_USER),
+        ]
     }
 
-    setConfigurationState(newConfigurationState);
-  };
-
-  function deleteEvent_internalcomponent(component: Component<BarrierEventTypes, BarrierPorts> | Component<BarrierEventTypes | SensorEventTypes | DSBEventTypes | UserEventTypes, DSB_Ports> | Component<UserEventTypes, UserPorts> , event: Event<UserEventTypes, UserPorts> & Event<BarrierEventTypes, BarrierPorts> & Event<BarrierEventTypes | SensorEventTypes | DSBEventTypes | UserEventTypes, DSB_Ports> & Event<UserEventTypes, UserPorts>) {
-    let newConfigurationState = {...configurationState};
-
-    const comp_state = newConfigurationState.componentState.get(component);
-    if (comp_state !== undefined) {
-      _.pull(comp_state.events, event);
+    let startState = {
+        componentState: new Map([
+            [barrier, barrier_state] as [any, any],
+            [sensor, sensor_startstate] as [any, any],
+            [DSB, DSB_state] as [any, any],
+            [user, user_startstate] as [any, any],
+        ]),
     }
 
-    setConfigurationState(newConfigurationState);
-  }
 
-  const deleteEvent = (component: Component<UserEventTypes, UserPorts> | Component<BarrierEventTypes, BarrierPorts> | Component<BarrierEventTypes | SensorEventTypes | DSBEventTypes | UserEventTypes, DSB_Ports>) => (event: Event<UserEventTypes, UserPorts> & Event<BarrierEventTypes, BarrierPorts> & Event<BarrierEventTypes | SensorEventTypes | DSBEventTypes | UserEventTypes, DSB_Ports>) => deleteEvent_internalcomponent(component, event);
-  
-  const compToEventTypeMap = new Map<Component<any, any>, object>();
-  compToEventTypeMap.set(barrier, BarrierEventTypes);
-  compToEventTypeMap.set(DSB, DSBEventTypes);
-  compToEventTypeMap.set(sensor, SensorEventTypes);
-  compToEventTypeMap.set(user, UserEventTypes);
-  
-  const classes = useStyles();
+    const [configurationState, setConfigurationState] = useState(_.cloneDeep(startState));
+    const [svg, setSVG] = useState("");
+    const [spaceSize, setSpaceSize] = useState(10);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              Sorrir - MVP
-            </Typography>
-            <Button variant="contained" onClick={() => {
-                setConfigurationState(configurationStep(configuration, configurationState));
-              }
-              }>Step</Button>
-            <form className={classes.root} noValidate autoComplete="off">
-              <TextField id="outlined-basic" label="Size" type="number" variant="outlined" size="small" defaultValue={spaceSize} onChange={(e) => setSpaceSize(+e.target.value)}/>
-            </form>
-            <Button variant="contained" onClick={createStateSpace}>
-              StateSpace</Button>
-            <Button variant="contained" onClick={reset}>Reset</Button>
-          </Toolbar>
-        </AppBar>
-        <div className="App-Canvas">
-          <GridList>
-           {configuration.components.map(c => {
-              return (
-                  <ComponentComp c={c} c_state={configurationState.componentState.get(c)} eventTypes={compToEventTypeMap.get(c) || {}} enqueue={enqueueEvent} dequeue={deleteEvent(c)}></ComponentComp>
-              )
-           })}
-          </GridList>
-          <hr/>
-          <div id="statespace" dangerouslySetInnerHTML={{__html:svg}}>
-          </div>
+    function createStateSpace() {
+        var data = depGraphToDot(stateSpace(configuration, configurationState, allConfigurationSteps, spaceSize));
+        // Generate the Visualization of the Graph into "svg".
+        var svg = Viz(data, "svg");
+        setSVG(svg);
+    }
+
+    function reset() {
+        setConfigurationState(_.cloneDeep(startState));
+        setSVG("");
+    }
+
+    function enqueueEvent(component: Component<BarrierEventTypes, BarrierPorts> | Component<BarrierEventTypes | SensorEventTypes | DSBEventTypes | UserEventTypes, DSB_Ports> | Component<UserEventTypes, UserPorts>, event: Event<BarrierEventTypes, BarrierPorts> & Event<BarrierEventTypes | SensorEventTypes | DSBEventTypes | UserEventTypes, DSB_Ports> & Event<UserEventTypes, UserPorts>) {
+        let newConfigurationState = {...configurationState};
+
+        const comp_state = newConfigurationState.componentState.get(component);
+        if (comp_state !== undefined) {
+            comp_state.events.push(event);
+        }
+
+        setConfigurationState(newConfigurationState);
+    };
+
+    function deleteEvent_internalcomponent(component: Component<BarrierEventTypes, BarrierPorts> | Component<BarrierEventTypes | SensorEventTypes | DSBEventTypes | UserEventTypes, DSB_Ports> | Component<UserEventTypes, UserPorts>, event: Event<UserEventTypes, UserPorts> & Event<BarrierEventTypes, BarrierPorts> & Event<BarrierEventTypes | SensorEventTypes | DSBEventTypes | UserEventTypes, DSB_Ports> & Event<UserEventTypes, UserPorts>) {
+        let newConfigurationState = {...configurationState};
+
+        const comp_state = newConfigurationState.componentState.get(component);
+        if (comp_state !== undefined) {
+            _.pull(comp_state.events, event);
+        }
+
+        setConfigurationState(newConfigurationState);
+    }
+
+    const deleteEvent = (component: Component<UserEventTypes, UserPorts> | Component<BarrierEventTypes, BarrierPorts> | Component<BarrierEventTypes | SensorEventTypes | DSBEventTypes | UserEventTypes, DSB_Ports>) => (event: Event<UserEventTypes, UserPorts> & Event<BarrierEventTypes, BarrierPorts> & Event<BarrierEventTypes | SensorEventTypes | DSBEventTypes | UserEventTypes, DSB_Ports>) => deleteEvent_internalcomponent(component, event);
+
+    const compToEventTypeMap = new Map<Component<any, any>, object>();
+    compToEventTypeMap.set(barrier, BarrierEventTypes);
+    compToEventTypeMap.set(DSB, DSBEventTypes);
+    compToEventTypeMap.set(sensor, SensorEventTypes);
+    compToEventTypeMap.set(user, UserEventTypes);
+
+    const classes = useStyles();
+
+    return (
+        <div>
+            <CssBaseline/>
+
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Typography variant="h6">
+                        SORRIR Demo
+                    </Typography>
+
+                    <div className={classes.grow}/>
+
+                    <Button color="inherit" onClick={() => {
+                        setConfigurationState(configurationStep(configuration, configurationState));
+                    }
+                    }>
+                        Step
+                    </Button>
+                    <Button color="inherit" onClick={reset}>Reset</Button>
+
+                    <div className={classes.grow}/>
+
+                    <form noValidate autoComplete="off">
+                        <TextField InputProps={{className: classes.inputColor}} InputLabelProps={{className: classes.inputColor}} id="outlined-basic" label="Size" type="number" variant="outlined" size="small"
+                                   defaultValue={spaceSize} onChange={(e) => setSpaceSize(+e.target.value)}/>
+                    </form>
+                    <Button color="inherit" onClick={createStateSpace}>
+                        StateSpace
+                    </Button>
+                </Toolbar>
+            </AppBar>
+            <Toolbar/>
+
+            <main className={classes.content}>
+                <Grid container justify="center" spacing={3}>
+                    {configuration.components.map(c => {
+                        return (
+                            <Grid item>
+                                <ComponentComp c={c} c_state={configurationState.componentState.get(c)}
+                                               eventTypes={compToEventTypeMap.get(c) || {}} enqueue={enqueueEvent}
+                                               dequeue={deleteEvent(c)}>
+
+                                </ComponentComp>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+
+                <Container>
+                    <div id="statespace" dangerouslySetInnerHTML={{__html: svg}}>
+                    </div>
+                </Container>
+            </main>
         </div>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Component.tsx
+++ src/components/Component.tsx
@@ -5,141 +5,147 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import AddBoxIcon from '@material-ui/icons/AddBox';
 import DeleteIcon from '@material-ui/icons/Delete';
-import React, { useState } from 'react';
-import { AbstractState, Component, Event, StateMachineState, Port } from 'sorrir-framework';
+import React, {useState} from 'react';
+import {AbstractState, Component, Event, StateMachineState, Port} from 'sorrir-framework';
 
 
-const useStyles = makeStyles((theme: Theme) => 
-  createStyles({
-    card: {
-      margin: theme.spacing(1),
-      minWidth: 275,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
-    },
-  })
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        card: {
+            margin: theme.spacing(2),
+            minWidth: 275,
+        },
+        bullet: {
+            display: 'inline-block',
+            margin: '0 2px',
+            transform: 'scale(0.8)',
+        },
+        title: {
+            fontSize: 14,
+        },
+        pos: {
+            marginBottom: 12,
+        },
+        formControl: {
+            margin: theme.spacing(1),
+            minWidth: 120,
+        },
+    })
 );
 
-const StateMachineStateComp: React.FC<{s: StateMachineState<any, any, any, any>}> = (props) => {
+const StateMachineStateComp: React.FC<{ s: StateMachineState<any, any, any, any> }> = (props) => {
 
     return (
-      <div>
-        <p>Discrete State: {JSON.stringify(props.s.state.fsm)}</p>
-        <p>Complex State: {JSON.stringify(props.s.state.my)}</p>
-      </div>
+        <div>
+            <p>Discrete State: {JSON.stringify(props.s.state.fsm)}</p>
+            <p>Complex State: {JSON.stringify(props.s.state.my)}</p>
+        </div>
     )
-  }
-  
-  function isStateMachineState(state: AbstractState<any, any, any> | StateMachineState<any, any, any, any> | undefined) : state is StateMachineState<any, any, any, any> {
+}
+
+function isStateMachineState(state: AbstractState<any, any, any> | StateMachineState<any, any, any, any> | undefined): state is StateMachineState<any, any, any, any> {
     return (state as StateMachineState<any, any, any, any>).state.fsm !== undefined;
-  }
+}
 
-  interface EventCompProps {
-    events: Event<any,any>[] | undefined,
-    dequeue: (event: Event<any,any>) => void,
-  }
+interface EventCompProps {
+    events: Event<any, any>[] | undefined,
+    dequeue: (event: Event<any, any>) => void,
+}
 
-  const EventComp: React.FC<EventCompProps> = (props) => {
-      return (
+const EventComp: React.FC<EventCompProps> = (props) => {
+    return (
         <div className="Component-EventBox">
             <div>
-            Events:
-            </div> 
+                Events:
+            </div>
             <div className="Component-EventList">
-            {props.events && props.events.map(e => {
-                return (
-                    <div>
-                    {e.port}.{e.type}
-                    <Button size="small" onClick={() => props.dequeue(e)}>
-                      <DeleteIcon/>
-                    </Button>
-                    </div>
-                )
-            })}
+                {props.events && props.events.map(e => {
+                    return (
+                        <div>
+                            {e.port}.{e.type}
+                            <Button size="small" onClick={() => props.dequeue(e)}>
+                                <DeleteIcon/>
+                            </Button>
+                        </div>
+                    )
+                })}
             </div>
         </div>
-      )
-  }
+    )
+}
 
-  export interface ComponentCompProps {
-    c: Component<any,any>,
-    c_state: AbstractState<any, any, any> | undefined,
-    eventTypes:{[key:string]: any},
-    enqueue: (component: Component<any, any>, event: Event<any,any>) => void,
-    dequeue: (event: Event<any,any>) => void
-  }
-  
-  export const ComponentComp: React.FC<ComponentCompProps> = (props) => {
-  
+export interface ComponentCompProps {
+    c: Component<any, any>,
+    c_state: AbstractState<any, any, any> | undefined,
+    eventTypes: { [key: string]: any },
+    enqueue: (component: Component<any, any>, event: Event<any, any>) => void,
+    dequeue: (event: Event<any, any>) => void
+}
+
+export const ComponentComp: React.FC<ComponentCompProps> = (props) => {
+
     const [portToSendEvent, setPortToSendEvent] = useState("");
     const [eventToSend, setEventToSend] = useState("");
 
     const classes = useStyles();
-  
+
     return (
-        <Card className={classes.card} variant="outlined">
+        <Card className={classes.card}>
             <CardContent>
-                    Component: {props.c.name}
+                Component: {props.c.name}
                 {
-                    (isStateMachineState(props.c_state)) ? (<StateMachineStateComp s={props.c_state}></StateMachineStateComp>) : JSON.stringify(props.c_state)
+                    (isStateMachineState(props.c_state)) ? (<StateMachineStateComp
+                        s={props.c_state}></StateMachineStateComp>) : JSON.stringify(props.c_state)
                 }
                 <EventComp events={props.c_state?.events} dequeue={props.dequeue}/>
                 <div className="Component-EventEnqueue">
-                <FormControl className={classes.formControl}>
-                  <InputLabel id="port-select-label">Port:</InputLabel>
-                  <Select
-                    labelId="port-select-label"
-                    id="port-select"
-                    value={portToSendEvent}
-                    onChange={(event: React.ChangeEvent<{ value: unknown }>) => {setPortToSendEvent(event.target.value as string)}} 
-                  >
-                      <MenuItem value={undefined}>
-                          <em>Internal</em>
-                      </MenuItem>
-                      {props.c.ports.map((p:Port<any,any>) => {
-                          return (
-                              <MenuItem value={p.name}>{p.name}</MenuItem>
-                          )
-                      })}
-                  </Select>
-                </FormControl>
-                <FormControl className={classes.formControl}>
-                  <InputLabel id="event-select-label">Event:</InputLabel>
-                  <Select
-                    labelId="event-select-label"
-                    id="event-select"
-                    value={eventToSend}
-                    onChange={(event: React.ChangeEvent<{ value: unknown }>) => setEventToSend(event.target.value as string)} 
-                  >
-                      {
-                        Object.keys(props.eventTypes).map(e => {
-                          return (
-                              <MenuItem value={e.toString()}>{e.toString()}</MenuItem>
-                          )
-                      })}
-                  </Select>
-                </FormControl>
-                <Button size="small" onClick={ () => 
-                props.enqueue(props.c, {type: props.eventTypes[eventToSend], port: portToSendEvent} as Event<any,any>)
-              }><AddBoxIcon/></Button>
-              </div>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="port-select-label">Port:</InputLabel>
+                        <Select
+                            labelId="port-select-label"
+                            id="port-select"
+                            value={portToSendEvent}
+                            onChange={(event: React.ChangeEvent<{ value: unknown }>) => {
+                                setPortToSendEvent(event.target.value as string)
+                            }}
+                        >
+                            <MenuItem value={undefined}>
+                                <em>Internal</em>
+                            </MenuItem>
+                            {props.c.ports.map((p: Port<any, any>) => {
+                                return (
+                                    <MenuItem value={p.name}>{p.name}</MenuItem>
+                                )
+                            })}
+                        </Select>
+                    </FormControl>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="event-select-label">Event:</InputLabel>
+                        <Select
+                            labelId="event-select-label"
+                            id="event-select"
+                            value={eventToSend}
+                            onChange={(event: React.ChangeEvent<{ value: unknown }>) => setEventToSend(event.target.value as string)}
+                        >
+                            {
+                                Object.keys(props.eventTypes).map(e => {
+                                    return (
+                                        <MenuItem value={e.toString()}>{e.toString()}</MenuItem>
+                                    )
+                                })}
+                        </Select>
+                    </FormControl>
+                    <Button size="small" onClick={() =>
+                        props.enqueue(props.c, {
+                            type: props.eventTypes[eventToSend],
+                            port: portToSendEvent
+                        } as Event<any, any>)
+                    }><AddBoxIcon/></Button>
+                </div>
             </CardContent>
-      </Card>
+        </Card>
     )
-  }
+}

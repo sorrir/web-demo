686ad10acf3c64324edd7d519986e6bbb95e877e
David <david.moedinger@ketzu.net>
2020-05-07 14:26:16 +0200

    making it actually callable on random urls.

---
+++ docs/precache-manifest.59e06b8733e3ac140365214c2d325291.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3792a70232963fe4b2ae21cd0d47b949",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7abfd8434d2a011eb686",
+    "url": "./static/css/main.f39b8463.chunk.css"
+  },
+  {
+    "revision": "ee0a5692e322bd4b1901",
+    "url": "./static/js/2.2ba942e3.chunk.js"
+  },
+  {
+    "revision": "d90e379e231c1b93e398a8cad7edd695",
+    "url": "./static/js/2.2ba942e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7abfd8434d2a011eb686",
+    "url": "./static/js/main.0d608f04.chunk.js"
+  },
+  {
+    "revision": "5bfe314e947635bb4458",
+    "url": "./static/js/runtime-main.29666609.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50d03da3672f3901c776081a2f822cc8.js"
+  "./precache-manifest.59e06b8733e3ac140365214c2d325291.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
